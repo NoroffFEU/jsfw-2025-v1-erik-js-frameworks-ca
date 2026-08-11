8b8bbb0f171eac1111d437c4750bea089a9dc21e
evrlong <127796398+evrlong@users.noreply.github.com>
2026-08-11 19:48:24 +0200

    footer redesign

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 export function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto text-center">
-        &copy; {new Date().getFullYear()} FrameShop. All rights reserved.
+    <footer className="mt-6 border-b border-gray-400 bg-gray-200 py-4 sm:py-8">
+      <div className="container mx-auto text-center text-gray-700 text-sm sm:text-base">
+        {" "}
+        All rights reserved &copy; {new Date().getFullYear()}
+        <span className="font-bold"> FrameShop.</span>
       </div>
     </footer>
   );
